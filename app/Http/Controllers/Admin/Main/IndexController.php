5eef0c5579d5e91b6af07a68fad3ba2b3749aeef
Dmytriy <md2263815@gmail.com>
2023-02-22 12:54:25 +0200

    commit

---
+++ app/Http/Controllers/Admin/Main/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Main;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        $users = User::all();
+        $posts = Post::all();
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin.main.index', compact('users', 'posts', 'categories', 'tags'));
+    }
+}
